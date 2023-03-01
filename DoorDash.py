b66813f3aac100f19906c8176bba08d276b8bd58
Jared Rosenlund <jared.rosenlund17@gmail.com>
2023-02-28 17:52:19 -0700

    Role Assignments

--- DoorDash.py
+++ DoorDash.py
@@ -1,5 +1,14 @@
 # Description: File for the group project
-# Authors: 
+# Authors: Ben, Brad, Devan, Jared, Miles, Spencer
+
+# Create Order class: Miles
+# Create Person class: Spencer
+# Create Customer (Person) class: Devan
+# Create Queue: Brad
+# Create Dictionary: Jared
+# Print and Sort: Ben
+
+# Goal: Be done by Monday, Mar 6
 
 print("Hey guys :)")
 print("Testing")
