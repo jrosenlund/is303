51b0d7fe2ab0b653ce27557dc1f9ea4c793044f6
Ben Brittain <benjaminpbrittain@gmail.com>
2023-03-05 21:42:19 -0700

    Person Class

--- DoorDash.py
+++ DoorDash.py
@@ -14,3 +14,23 @@
 print("Hey guys :)")
 print("Testing")
 print("This is the new stuff.")
+
+# HEAD
+print("This is another test")
+# =======
+# Get random module
+import random
+
+# Class for Person
+class Person():
+    # Constructor
+    def __init__(self,customer_name):
+        self.customer_name = self.randomName()
+    
+    # Random name method
+    def randomName(self):
+        # Create customer list
+        asCustomers = ["Jefe", "El Guapo", "Lucky Day", "Ned Nederlander", "Dusty Bottoms", "Harry Flugleman", "Carmen", "Invisible Swordsman", "Singing Bush"]
+        # Choose and return random name
+        return random.choice(asCustomers)
+# >>>>>>> 4194a83de4e821f5deb025d3a18f1341763ee0c1
